ee2f589bd151c1188372656593027691011a5c64
wuonly <wuonly@gmail.com>
2016-08-15 14:15:46 +0800

    jquery.sinaEmotion
    jquery.sinaEmotion

---
+++ jquery.sinaEmotion.js
@@ -0,0 +1,217 @@
+var uSinaEmotions = {
+	"[微笑]" : "huanglianwx_thumb.gif",
+	"[嘻嘻]" : "tootha_thumb.gif",
+	"[哈哈]" : "laugh.gif",
+	"[可爱]" : "tza_thumb.gif",
+	"[可怜]" : "kl_thumb.gif",
+	"[挖鼻]" : "wabi_thumb.gif",
+	"[吃惊]" : "cj_thumb.gif",
+	"[害羞]" : "shamea_thumb.gif",
+	"[挤眼]" : "zy_thumb.gif",
+	"[闭嘴]" : "bz_thumb.gif",
+	"[鄙视]" : "bs2_thumb.gif",
+	"[爱你]" : "lovea_thumb.gif",
+	"[泪]" : "sada_thumb.gif",
+	"[偷笑]" : "heia_thumb.gif",
+	"[亲亲]" : "qq_thumb.gif",
+	"[生病]" : "sb_thumb.gif",
+	"[太开心]" : "mb_thumb.gif",
+	"[白眼]" : "landeln_thumb.gif",
+	"[右哼哼]" : "yhh_thumb.gif",
+	"[左哼哼]" : "zhh_thumb.gif",
+	"[嘘]" : "x_thumb.gif",
+	"[衰]" : "cry.gif",
+	"[委屈]" : "wq_thumb.gif",
+	"[吐]" : "t_thumb.gif",
+	"[哈欠]" : "haqianv2_thumb.gif",
+	"[抱抱]" : "bba_thumb.gif",
+	"[怒]" : "angrya_thumb.gif",
+	"[疑问]" : "yw_thumb.gif",
+	"[馋嘴]" : "cza_thumb.gif",
+	"[拜拜]" : "88_thumb.gif",
+	"[思考]" : "sk_thumb.gif",
+	"[汗]" : "sweata_thumb.gif",
+	"[困]" : "kunv2_thumb.gif",
+	"[睡]" : "huangliansj_thumb.gif",
+	"[钱]" : "money_thumb.gif",
+	"[失望]" : "sw_thumb.gif",
+	"[酷]" : "cool_thumb.gif",
+	"[色]" : "huanglianse_thumb.gif",
+	"[哼]" : "hatea_thumb.gif",
+	"[鼓掌]" : "gza_thumb.gif",
+	"[晕]" : "dizzya_thumb.gif",
+	"[悲伤]" : "bs_thumb.gif",
+	"[抓狂]" : "crazya_thumb.gif",
+	"[黑线]" : "h_thumb.gif",
+	"[阴险]" : "yx_thumb.gif",
+	"[怒骂]" : "numav2_thumb.gif",
+	"[互粉]" : "hufen_thumb.gif",
+	"[心]" : "hearta_thumb.gif",
+	"[伤心]" : "unheart.gif",
+	"[猪头]" : "pig.gif",
+	"[熊猫]" : "panda_thumb.gif",
+	"[兔子]" : "rabbit_thumb.gif",
+	"[ok]" : "ok_thumb.gif",
+	"[耶]" : "ye_thumb.gif",
+	"[good]" : "good_thumb.gif",
+	"[NO]" : "buyao_org.gif",
+	"[赞]" : "z2_thumb.gif",
+	"[来]" : "come_thumb.gif",
+	"[弱]" : "sad_thumb.gif",
+	"[草泥马]" : "shenshou_thumb.gif",
+	"[神马]" : "horse2_thumb.gif",
+	"[囧]" : "j_thumb.gif",
+	"[浮云]" : "fuyun_thumb.gif",
+	"[给力]" : "geiliv2_thumb.gif",
+	"[围观]" : "wg_thumb.gif",
+	"[威武]" : "vw_thumb.gif",
+	"[奥特曼]" : "otm_thumb.gif",
+	"[礼物]" : "liwu_thumb.gif",
+	"[钟]" : "clock_thumb.gif",
+	"[话筒]" : "huatongv2_thumb.gif",
+	"[蜡烛]" : "lazhuv2_thumb.gif",
+	"[蛋糕]" : "cakev2_thumb.gif",
+	"[牛郎]" : "qixi2016_niulang2_thumb.gif",
+	"[织女]" : "qixi2016_zhinv2_thumb.gif",
+	"[最右]" : "lxhzuiyou_thumb.gif",
+	"[泪流满面]" : "lxhtongku_thumb.gif",
+	"[江南style]" : "gangnamstyle_thumb.gif",
+	"[偷乐]" : "lxhtouxiao_thumb.gif",
+	"[加油啊]" : "lxhjiayou_thumb.gif",
+	"[doge]" : "doge_thumb.gif",
+	"[喵喵]" : "mm_thumb.gif",
+	"[笑cry]" : "xiaoku_thumb.gif",
+	"[足球]" : "football.gif",
+	"[炸鸡和啤酒]" : "zhaji_thumb.gif",
+	"[快银]" : "xman_kuaiyin_org.gif",
+	"[暴风女]" : "xman_baofengnv_thumb.gif",
+	"[芒果流口水]" : "mango_07_thumb.gif",
+	"[芒果点赞]" : "mango_12_thumb.gif",
+	"[芒果大笑]" : "mango_02_thumb.gif",
+	"[芒果得意]" : "mango_03_thumb.gif",
+	"[芒果萌萌哒]" : "mango_11_thumb.gif",
+	"[羊年大吉]" : "yangniandj_thumb.gif",
+	"[西瓜]" : "watermelon.gif",
+	"[老妈我爱你]" : "mothersday_thumb.gif",
+	"[母亲节]" : "carnation_thumb.gif",
+	"[肥皂]" : "soap_thumb.gif",
+	"[有钱]" : "youqian_thumb.gif",
+	"[地球一小时]" : "earth1r_thumb.gif",
+	"[国旗]" : "flag_thumb.gif",
+	"[许愿]" : "lxhxuyuan_thumb.gif",
+	"[风扇]" : "fan.gif",
+	"[雪]" : "snow_thumb.gif",
+	"[马上有对象]" : "mashangyouduixiang_thumb.gif",
+	"[马到成功旧]" : "madaochenggong_thumb.gif",
+	"[青啤鸿运当头]" : "hongyun_thumb.gif",
+	"[让红包飞]" : "hongbaofei2014_thumb.gif",
+	"[ali做鬼脸]" : "alizuoguiliannew_thumb.gif",
+	"[ali哇]" : "aliwanew_thumb.gif",
+	"[xkl转圈]" : "xklzhuanquan_thumb.gif",
+	"[酷库熊顽皮]" : "kxwanpi_thumb.gif",
+	"[bm可爱]" : "bmkeai_thumb.gif",
+	"[BOBO爱你]" : "boaini_thumb.gif",
+	"[转发]" : "lxhzhuanfa_thumb.gif",
+	"[得意地笑]" : "lxhdeyidixiao_thumb.gif",
+	"[ppb鼓掌]" : "ppbguzhang_thumb.gif",
+	"[din推撞]" : "dintuizhuang_thumb.gif",
+	"[moc转发]" : "moczhuanfa_thumb.gif",
+	"[lt切克闹]" : "ltqiekenao_thumb.gif",
+	"[笑哈哈]" : "lxhwahaha_thumb.gif",
+}
+
+// 替换
+function AnalyticEmotion(s, path) {
+	if (typeof (s) != "undefined") {
+		var sArr = s.match(/\[.*?\]/g);
+		if (sArr == null)
+			return s;
+		for (var i = 0; i < sArr.length; i++) {
+
+			var reStr = "<img src=\"" + path + "/images/emotions/" + getJsonValue(uSinaEmotions, sArr[i])
+					+ "\" style='width:22px;height:22px;' />";
+			s = s.replace(sArr[i], reStr);
+		}
+	}
+	return s;
+}
+
+function getJsonValue(jsonObj, key) {
+	var re = "";
+	$.each(jsonObj, function(name, value) {
+		if (name == key) {
+			re = value;
+		}
+	});
+	return re;
+}
+
+function preLoadImg(url) {
+	var img = new Image();
+	img.src = url;
+}
+
+(function($) {
+	$.fn.SinaEmotion = function(target, imgpath) {
+		var cat_current;
+		var cat_page;
+		$(this).click(function(event) {
+			event.stopPropagation();
+
+			$('body').append('<div class="emotionsCss"></div>');
+
+			$('.emotionsCss').html('<div>正在加载，请稍候...</div>');
+			$('.emotionsCss').click(function(event) {
+				event.stopPropagation();
+			});
+
+			$('.emotionsCss').html('<div class="emcontainer"></div>');
+			showEmotions();
+			var eTop = target.offset().top - $('.emotionsCss').height();
+			console.log($('.emotionsCss').height());
+			var eLeft = target.offset().left - 50;
+			$('.emotionsCss').css({
+				top : eTop,
+				left : eLeft
+			});
+		});
+
+		$.fn.insertText = function(text) {
+			this.each(function() {
+				if (this.tagName !== 'INPUT' && this.tagName !== 'TEXTAREA') {
+					return;
+				}
+				if (document.selection) {
+					this.focus();
+					var cr = document.selection.createRange();
+					cr.text = text;
+					cr.collapse();
+					cr.select();
+				} else if (this.selectionStart || this.selectionStart == '0') {
+					var start = this.selectionStart, end = this.selectionEnd;
+					this.value = this.value.substring(0, start) + text + this.value.substring(end, this.value.length);
+					this.selectionStart = this.selectionEnd = start + text.length;
+				} else {
+					this.value += text;
+				}
+			});
+			return this;
+		}
+
+		function showEmotions() {
+			$('.emotionsCss .emcontainer').html('');
+			$.each(uSinaEmotions, function(name, value) {
+				$('.emotionsCss .emcontainer').append(
+						$('<a class="a_emotions" href="javascript:void(0);" title="' + name + '"><img src="' + imgpath
+								+ '/images/emotions/' + value + '" alt="' + name + '" width="22" height="22" /></a>'));
+				preLoadImg(imgpath + '/images/emotions/' + value);
+			});
+			$('.a_emotions').click(function() {
+				target.insertText($(this).attr('title'));
+			});
+		}
+		$('body').click(function() {
+			$('.emotionsCss').remove();
+		});
+	}
+})(jQuery);
